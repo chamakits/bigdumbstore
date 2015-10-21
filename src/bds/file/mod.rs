bb4aaa3b7f3102f7bb6edef32ff88db2f6ac625e
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2015-10-21 03:20:10 -0400

    Able to find the key. Hurray!

--- src/bds/file/mod.rs
+++ src/bds/file/mod.rs
@@ -6,6 +6,7 @@ use std::io::prelude::*;
 
 //TODO do this in a smart way
 const KV_FILE: &'static str = "/home/chamakits/.config/big-dumb-store/.v6_store";
+const BUFF_SIZE: usize = 1024;
 
 //TODO change everything in this file from i32 to i64
 pub fn open_kv_file() -> File {
@@ -70,22 +71,15 @@ pub fn seek_key(file: &mut File, key_size: i32) -> u64 {
 
 //TODO doesn't work. Hurray
 pub fn read_key(file: &mut File, key_size: i32) -> String {
-    //let mut key_buffer = [0; (key_size+1) as usize];
-    let mut key_buffer = Vec::with_capacity( (key_size + 1) as usize);
-    let mut key_box = key_buffer.into_boxed_slice();
-    //let mut key_slice = &mut key_buffer;
-    //key_slice + 1;
-    println!("Key buffer:{:?}", key_box);
-    match file.read(&mut key_box) {
+    //TODO this is limited to only read data that fits in size. Improve
+    let mut key_buffer = [0; BUFF_SIZE];
+    let mut file_take = file.take(key_size as u64);
+    match file_take.read(&mut key_buffer) {
         Err(why) => panic!("Could not read size bytes. Err:{}",why),
         Ok(x) => {
-            println!("File read:{}", x);
+            println!("File read:{}", x)
         }
     }
-    key_buffer = key_box.into_vec();
-    let mut res = match String::from_utf8(key_buffer) {
-        Err(why) => panic!("Could not parse key string from bytes read. Err:{}", why),
-        Ok(str_parsed) => str_parsed,
-    };
+    let res = String::from_utf8_lossy(&mut key_buffer).into_owned();
     return res
 }

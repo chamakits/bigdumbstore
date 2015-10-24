262e8ce61888f43152c469deca4ffe0bbf6779d3
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2015-10-24 19:57:25 -0400

    Added function for creating directories if missing.

--- src/bds/runner/mod.rs
+++ src/bds/runner/mod.rs
@@ -1,34 +1,34 @@
 use super::file;
 use std::io;
 use std::fs;
+use std::path::Path;
 
 //TODO do this in a smart way
 //const KV_FILE: &'static str = "/home/chamakits/.config/big-dumb-store/.v6_store";
 //const KV_FILE: &'static str = "/home/chamakits/.config/big-dumb-store/.v6_store_rust";
 //const KV_FILE: &'static str = "/home/chamakits/.config/big-dumb-store/.v6_store_rust_struct";
-const KV_FILE: &'static str = "/home/chamakits/.config/big-dumb-store/.v7_store";
+//const KV_FILE: &'static str = "/home/chamakits/.config/big-dumb-store/.v7_store";
+const KV_FILE: &'static str = "/home/chamakits/.config-2/big-dumb-store/.v7_store";
 
 fn create_file_if_not_exist(kv_file_path_str: &str) {
-    /*
-    let kv_file_path_metadata = match fs::metadata(kv_file_path_str) {
-        Err(why) => panic!("Couldn't get file matadata. Err: {}", why),
-        Ok(file_metadata) => file_metadata
-    };
-    if !kv_file_path_metadata.is_file() && !kv_file_path_metadata.is_dir() {
-        match fs::File::create(kv_file_path_str) {
-            Err(why) => panic!("Couldn't create file. Err: {}", why),
-            _ => {}
-        }
-    }
-    */
+    let kv_file_path = Path::new(kv_file_path_str);
+
     match fs::metadata(kv_file_path_str) {
         Err(_) => {
+            create_directories_if_needed(kv_file_path.parent().unwrap());
             match fs::File::create(kv_file_path_str) {
                 Err(why) => panic!("Couldn't create file. Err: {}", why),
                 _ => {}
             }
         },
         _ => {}
+    };
+}
+
+fn create_directories_if_needed(path: &Path) {
+    match fs::create_dir_all(path) {
+        Err(why) => panic!("Couldn't create directory path {:?}: error:{}", path, why),
+        Ok(_) => {},
     };
 }
 

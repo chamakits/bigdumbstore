d0b5641b9ff3b3c7cfb8da6c071172849ce50e37
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2015-10-25 06:44:01 -0400

    Now able to read and write to files in home using a curly.

--- src/bds/runner/mod.rs
+++ src/bds/runner/mod.rs
@@ -2,6 +2,8 @@ use super::file;
 use std::io;
 use std::fs;
 use std::path::Path;
+use std::path::PathBuf;
+use std::env;
 
 //TODO do this in a smart way
 //const KV_FILE: &'static str = "/home/chamakits/.config/big-dumb-store/.v6_store";
@@ -13,11 +15,18 @@ const KV_FILE: &'static str = "/home/chamakits/.config-2/big-dumb-store/.v7_stor
 fn create_file_if_not_exist(kv_file_path_str: &str) {
     let kv_file_path = Path::new(kv_file_path_str);
 
+    debug!("kv_file_path: {:?}", kv_file_path);
+
     match fs::metadata(kv_file_path_str) {
         Err(_) => {
-            create_directories_if_needed(kv_file_path.parent().unwrap());
-            match fs::File::create(kv_file_path_str) {
-                Err(why) => panic!("Couldn't create file. Err: {}", why),
+            let kv_path_dirs_created =
+                create_directories_if_needed(kv_file_path.parent().unwrap());
+            let kv_path_joined = Path::new(&kv_path_dirs_created)
+                .join(kv_file_path.file_name().unwrap());
+            debug!("kv_path_dirs_created: {:?}", kv_path_joined);
+            match fs::File::create(kv_path_joined) {
+                Err(why) => panic!("Couldn't create file. Err: {}, for path: {}",
+                                   why, kv_file_path_str),
                 _ => {}
             }
         },
@@ -25,19 +34,60 @@ fn create_file_if_not_exist(kv_file_path_str: &str) {
     };
 }
 
-fn create_directories_if_needed(path: &Path) {
-    match fs::create_dir_all(path) {
+fn path_with_curly_to_abs(path_maybe_with_curly: &str) -> PathBuf {
+    let resolve_path_for_home = match path_maybe_with_curly.split_at(1) {
+        ("~",x) => {
+            let rest_of_path = x.split_at(1).1;
+            debug!("Found home directory, file:{}, {:?}",
+                   x, env::home_dir().unwrap().join(Path::new(rest_of_path)));
+            env::home_dir().unwrap().join(Path::new(rest_of_path))
+        },
+        _ => {
+            debug!("Not a home directory specified");
+            Path::new(path_maybe_with_curly).to_path_buf()
+        }
+    };
+    resolve_path_for_home
+}
+
+fn create_directories_if_needed(path: &Path) -> String {
+    debug!("create_directories_if_needed: {}", path.to_str().unwrap());
+    let path_str = path.to_str().unwrap();
+
+    if path_str.len() == 0 {
+        return path_str.to_string();
+    }
+    /*
+    let resolve_path_for_home = match path_str.split_at(1) {
+        ("~",x) => {
+            debug!("Found home directory");
+            env::home_dir().unwrap().join(Path::new(x))
+        },
+        _ => {
+            debug!("Not a home directory specified");
+            path.to_path_buf()
+        }
+    };
+    */
+    let resolve_path_for_home = path_with_curly_to_abs(path_str);
+    debug!("resolve_path_for_home: {:?}", resolve_path_for_home);
+
+    match fs::create_dir_all(&resolve_path_for_home) {
         Err(why) => panic!("Couldn't create directory path {:?}: error:{}", path, why),
         Ok(_) => {},
     };
+
+    return resolve_path_for_home.to_str().unwrap().to_string();
 }
 
 pub fn reading(read_args: Vec<String>, path: Option<String>) {
 
-    let path_str: String = match path {
+    let mut path_str: String = match path {
         Option::Some(_path_str) => _path_str.to_string(),
         Option::None => KV_FILE.to_string()
     };
+
+    path_str = path_with_curly_to_abs(&path_str).to_str().unwrap().to_string();
     
     debug!("Will be reading with args: {:?}, path: {:?}", read_args, path_str);    
     create_file_if_not_exist(&path_str);
@@ -54,10 +104,14 @@ pub fn reading(read_args: Vec<String>, path: Option<String>) {
 
 pub fn writing(write_args: Vec<String>, path: Option<String>) {
 
-    let path_str: String = match path {
+    let mut path_str: String = match path {
         Option::Some(_path_str) => _path_str.to_string(),
         Option::None => KV_FILE.to_string()
     };
+
+    path_str = path_with_curly_to_abs(&path_str).to_str().unwrap().to_string();
+
+    debug!("Will be writing with args: {:?}, path: {:?}", write_args, path_str);
     
     create_file_if_not_exist(&path_str);
     let key_to_write = write_args.get(0).unwrap();

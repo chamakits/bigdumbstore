0f3c5502bcad971bc20c4885c81f8964100538d0
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2015-11-06 23:34:19 -0500

    Making forcing of functions to be compiled even easier.

--- src/bds/runner/mod.rs
+++ src/bds/runner/mod.rs
@@ -218,14 +218,13 @@ mod tests {
     
     #[test]
     fn mock_forcing_functions_compiled() {
-        let _1 = super::determine_mode;
-        //let _1 = vec![|x| {super::determine_mode(x);}, |x:Vec<String>| {super::junk_writing;}];
-        let _2 = super::junk_writing;
-        let _3 = super::reading;
-        let _4 = super::writing;
-        let _5 = super::path_with_curly_to_abs;
-        let _6 = super::create_directories_if_needed;
-        
+        #![allow(path_statements)]
+        super::determine_mode;
+        super::junk_writing;
+        super::reading;
+        super::writing;
+        super::path_with_curly_to_abs;
+        super::create_directories_if_needed;
     }
 
     #[test]
